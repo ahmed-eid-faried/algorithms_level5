9394a6e18069452786af58185d397ab9eb4d5e14
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-08-29 09:43:54 +0300

    Add project files.

---
+++ algorithms_level5/clsDblLinkedList.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template<class T>
+class clsDblLinkedList {
+public:
+	struct Node {
+		T value;
+		Node* prev;
+		Node* next;
+	};
+
+
+
+
+
+};
