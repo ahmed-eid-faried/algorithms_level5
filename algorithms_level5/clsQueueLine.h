3a3ca8d0331ed14a254b607e8cd3e49743c8c5fe
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-02 01:19:11 +0300

    PrintTicketsLineRTL

--- algorithms_level5/clsQueueLine.h
+++ algorithms_level5/clsQueueLine.h
@@ -1,19 +1,117 @@
 #pragma once
 #include <iostream>
 #include  <queue>
+#include "clsDate.h"
+class clsTicket
+{
+private:
+
+	short _Number = 0;
+	string _Prefix;
+	string _TicketTime;
+	short _WaitingClients = 0;
+	short _AverageServeTime = 0;
+	short _ExpectedServeTime = 0;
+
+public:
+	clsTicket(string Prefix, short Number, short WaitingClients, short AverageServeTime)
+	{
+
+		_Number = Number;
+		_TicketTime = clsDate::GetSystemDateTimeString();
+		_Prefix = Prefix;
+		_WaitingClients = WaitingClients;
+		_AverageServeTime = AverageServeTime;
+	}
+
+	string Prefix()
+	{
+		return _Prefix;
+
+	}
+	short Number()
+	{
+		return _Number;
+	}
+
+	string FullNumber()
+	{
+		return _Prefix + to_string(_Number);
+	}
+
+	string TicketTime()
+	{
+		return _TicketTime;
+	}
+
+	short WaitingClients()
+	{
+		return _WaitingClients;
+	}
+
+	short ExpectedServeTime()
+	{
+		return _AverageServeTime * _WaitingClients;
+	}
+
+	void Print()
+	{
+		cout << "\n\t\t\t  _______________________\n";
+		cout << "\n\t\t\t\t    " << FullNumber();
+		cout << "\n\n\t\t\t    " << _TicketTime;
+		cout << "\n\t\t\t    Wating Clients = " << _WaitingClients;
+		cout << "\n\t\t\t      Serve Time In";
+		cout << "\n\t\t\t       " << ExpectedServeTime() << " Minutes.";
+		cout << "\n\t\t\t  _______________________\n";
+
+
+	}
+};
 class clsQueueLine {
 private:
-	queue<int> MyQueue;
-	string _Title = "";
-	int _mins = 0;
+	short _TotalTickets = 0;
+	short _AverageServeTime = 0;
+	string _Prefix = "";
+
 public:
-	clsQueueLine(string Title, int mins) {
-		_Title = Title;
-		_mins = mins;
+	queue<clsTicket>  QueueLine;
+	clsQueueLine(string Prefix, int AverageServeTime) {
+		_Prefix = Prefix;
+		_AverageServeTime = AverageServeTime;
 	}
-	void IssueTicket() {}
-	void PrintInfo() {}
-	void PrintTicketsLineRTL() {}
+	void IssueTicket() {
+		_TotalTickets++;
+		clsTicket ticket = clsTicket(
+			_Prefix, _TotalTickets, QueueLine.size() - 1, _AverageServeTime
+		);
+		QueueLine.push(ticket);
+	}
+
+	void PrintInfo() {
+		cout << "_________________________________" << endl;
+		cout << "            Queue info           " << endl;
+		cout << "_________________________________" << endl;
+		cout << "   Prefix          =  " << _Prefix << endl;
+		//cout << "   Prefix  =  " << _AverageServeTime << endl;
+		cout << "   Total Tickets   =  " << _TotalTickets << endl;
+		cout << "   Served Clients  =  " << _TotalTickets - QueueLine.size() << endl;
+		cout << "   Waiting Clients =  " << QueueLine.size() - 1 << endl;
+		cout << "_________________________________" << endl;
+
+	}
+	void PrintTicketsLineRTL() {
+		if (QueueLine.empty())
+			cout << "\nTickets: No Tickets.";
+		else
+			cout << "\nTickets: ";
+		queue<clsTicket>  _Temp = QueueLine;
+		while (!_Temp.empty())
+		{
+			cout << " " << _Temp.front().FullNumber() << " <-- ";
+ 			_Temp.pop();
+		}
+		cout << endl;
+	}
 	void PrintTicketsLineLTR() {}
 	void PrintAllTickets() {}
 	void ServeNextClient() {}
--- algorithms_level5/clsQueueLineEx.h
+++ algorithms_level5/clsQueueLineEx.h
@@ -1,1 +1,54 @@
 #pragma once
+#include <iostream>
+#include "clsQueueLine.h"
+
+using namespace std;
+
+void clsQueueLineEx()
+{
+
+    clsQueueLine PayBillsQueue("A0", 10);
+    clsQueueLine SubscriptionsQueue("B0", 5);
+
+
+    PayBillsQueue.IssueTicket();
+    PayBillsQueue.IssueTicket();
+    PayBillsQueue.IssueTicket();
+    PayBillsQueue.IssueTicket();
+    PayBillsQueue.IssueTicket();
+
+    cout << "\nPay Bills Queue Info:\n";
+    PayBillsQueue.PrintInfo();
+
+    PayBillsQueue.PrintTicketsLineRTL();
+    PayBillsQueue.PrintTicketsLineLTR();
+
+
+
+    PayBillsQueue.PrintAllTickets();
+
+    PayBillsQueue.ServeNextClient();
+    cout << "\nPay Bills Queue After Serving One client\n";
+    PayBillsQueue.PrintInfo();
+
+    cout << "\nSubscriptions Queue Info:\n";
+
+    SubscriptionsQueue.IssueTicket();
+    SubscriptionsQueue.IssueTicket();
+    SubscriptionsQueue.IssueTicket();
+
+
+    SubscriptionsQueue.PrintInfo();
+
+    SubscriptionsQueue.PrintTicketsLineRTL();
+    SubscriptionsQueue.PrintTicketsLineLTR();
+
+    SubscriptionsQueue.PrintAllTickets();
+
+    SubscriptionsQueue.ServeNextClient();
+    cout << "\nSubscriptions Queue After Serving One client\n";
+    SubscriptionsQueue.PrintInfo();
+
+
+ }
+
