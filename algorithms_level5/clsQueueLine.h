57b713b98c2fb9d83ea50070261c5ca0ab0865ff
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-02 00:47:59 +0300

    clsQueueLine constractor

--- algorithms_level5/clsQueueLine.h
+++ algorithms_level5/clsQueueLine.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <iostream>
+#include  <queue>
+class clsQueueLine {
+private:
+	queue<int> MyQueue;
+	string _Title = "";
+	int _mins = 0;
+public:
+	clsQueueLine(string Title, int mins) {
+		_Title = Title;
+		_mins = mins;
+	}
+	void IssueTicket() {}
+	void PrintInfo() {}
+	void PrintTicketsLineRTL() {}
+	void PrintTicketsLineLTR() {}
+	void PrintAllTickets() {}
+	void ServeNextClient() {}
+};
